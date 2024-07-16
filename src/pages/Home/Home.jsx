64917981c0b1e7ea2ccbecf3c80ae4f46adacf8c
Rafaeldev22 <rafael.dev.22@gmail.com>
2024-07-16 14:44:04 -0300

    inicio do projeto portfolio pouchain

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css"
+
+export default function Home(){
+    return(
+        <div className="container">
+            <img src="https://pouchain.com.br/wp-content/uploads/2021/12/LOGO-branco-png.png" alt="" />
+            <div className="links">
+                <button>Papeis</button>
+                <button>Alças</button>
+                <button>Acabamentos</button>
+            </div>
+        </div>
+    )
+}
